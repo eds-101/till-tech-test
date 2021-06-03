491f7138aa1d922cd8bc253a7662674849d72b5d
Eds <steadyede@live.co.uk>
2021-06-03 14:53:32 +0100

    setup files

---
+++ spec/TillSpec.js
@@ -0,0 +1,12 @@
+describe("Hipster Till", function() {
+  let till;
+
+  beforeEach(function() {
+    till = new Till();
+  });
+
+  it("should print the date and time the receipt was ordered", function() {
+
+  });
+
+});
